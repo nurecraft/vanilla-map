58fafff2c61ef3bd4bf5870fc911a3ff7ebbadc8
Dmytro Hoi <me@dmytrohoi.com>
2020-05-03 22:53:49 +0300

    Initial commit

---
+++ overviewerConfig.js
@@ -0,0 +1,71 @@
+var overviewerConfig = {
+    "worlds": [
+        "nurecraft - overworld"
+    ],
+    "map": {
+        "controls": {
+            "overlays": true,
+            "spawn": true,
+            "coordsBox": true,
+            "mapType": true,
+            "compass": true,
+            "pan": true,
+            "zoom": true
+        },
+        "cacheTag": "1588533939",
+        "debug": true,
+        "north_direction": "lower-left"
+    },
+    "CONST": {
+        "UPPERRIGHT": 1,
+        "LOWERRIGHT": 2,
+        "UPPERLEFT": 0,
+        "tileSize": 384,
+        "LOWERLEFT": 3,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "spawnMarker": "icons/marker_home.png",
+            "queryMarker2x": "icons/marker_location_2x.png",
+            "spawnMarker2x": "icons/marker_home_2x.png",
+            "bedMarker": "bed.png",
+            "signMarker": "signpost_icon.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "2": "compass_lower-right.png",
+                "3": "compass_lower-left.png"
+            },
+            "queryMarker": "icons/marker_location.png"
+        },
+        "mapDivId": "mcmap"
+    },
+    "tilesets": [
+        {
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "poititle": "Markers",
+            "last_rendertime": 1588533659,
+            "defaultZoom": 1,
+            "zoomLevels": 8,
+            "path": "nurecraft",
+            "maxZoom": 8,
+            "spawn": [
+                83,
+                96,
+                965
+            ],
+            "world": "nurecraft - overworld",
+            "name": "NURECRAFT Daytime",
+            "isOverlay": false,
+            "showlocationmarker": true,
+            "imgextension": "jpg",
+            "center": [
+                83,
+                96,
+                965
+            ],
+            "minZoom": 0,
+            "north_direction": 0
+        }
+    ]
+};
