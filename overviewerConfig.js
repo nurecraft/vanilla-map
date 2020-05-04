c87808a8164400cd28c063b8931e39eb270c503c
root <root@vmi383959.contaboserver.net>
2020-05-05 01:09:06 +0200

    2020-05-05 Day Save

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -1,71 +1,71 @@
 var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "icons/marker_home.png",
+            "spawnMarker2x": "icons/marker_home_2x.png",
+            "queryMarker": "icons/marker_location.png",
+            "queryMarker2x": "icons/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "nurecraft - overworld"
+    ],
+    "map": {
+        "debug": true,
+        "cacheTag": "1588633731",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
     "tilesets": [
         {
+            "name": "NURECRAFT Daytime",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "nurecraft",
+            "base": "",
+            "bgcolor": "#1a1a1a",
             "world": "nurecraft - overworld",
-            "north_direction": 0,
-            "showlocationmarker": true,
-            "minZoom": 0,
+            "last_rendertime": 1588633045,
+            "imgextension": "jpg",
+            "isOverlay": false,
             "poititle": "Markers",
+            "showlocationmarker": true,
             "center": [
                 83,
                 96,
                 965
             ],
+            "minZoom": 0,
             "spawn": [
                 83,
                 96,
                 965
             ],
-            "name": "NURECRAFT Daytime",
-            "base": "",
-            "last_rendertime": 1588585625,
-            "path": "nurecraft",
-            "imgextension": "jpg",
-            "zoomLevels": 11,
-            "isOverlay": false,
-            "bgcolor": "#1a1a1a",
-            "maxZoom": 11,
-            "defaultZoom": 1
+            "north_direction": 0
         }
-    ],
-    "worlds": [
-        "nurecraft - overworld"
-    ],
-    "CONST": {
-        "LOWERLEFT": 3,
-        "UPPERRIGHT": 1,
-        "mapDivId": "mcmap",
-        "tileSize": 384,
-        "UPPERLEFT": 0,
-        "image": {
-            "spawnMarker": "icons/marker_home.png",
-            "signMarker": "signpost_icon.png",
-            "queryMarker2x": "icons/marker_location_2x.png",
-            "compass": {
-                "0": "compass_upper-left.png",
-                "1": "compass_upper-right.png",
-                "2": "compass_lower-right.png",
-                "3": "compass_lower-left.png"
-            },
-            "queryMarker": "icons/marker_location.png",
-            "bedMarker": "bed.png",
-            "defaultMarker": "signpost.png",
-            "spawnMarker2x": "icons/marker_home_2x.png"
-        },
-        "LOWERRIGHT": 2
-    },
-    "map": {
-        "controls": {
-            "overlays": true,
-            "compass": true,
-            "mapType": true,
-            "zoom": true,
-            "coordsBox": true,
-            "pan": true,
-            "spawn": true
-        },
-        "north_direction": "lower-left",
-        "cacheTag": "1588586718",
-        "debug": true
-    }
+    ]
 };
