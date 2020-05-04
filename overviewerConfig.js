8f0d26037bf6db157c580334cdb66ebbfaa8d663
root <root@vmi383959.contaboserver.net>
2020-05-05 01:18:21 +0200

    2020-05-05 Day Save

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1588633731",
+        "cacheTag": "1588634299",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "nurecraft - overworld",
-            "last_rendertime": 1588633045,
+            "last_rendertime": 1588634096,
             "imgextension": "jpg",
             "isOverlay": false,
             "poititle": "Markers",
