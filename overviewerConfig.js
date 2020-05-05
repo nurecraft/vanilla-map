c129b0286edebf769b6efdfe4812cb9f07c5361e
server-nurecraft <server@nurecraft.ml>
2020-05-05 19:29:55 +0200

    2020-05-05-19-29 Map Save

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1588688199",
+        "cacheTag": "1588699734",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "nurecraft - overworld",
-            "last_rendertime": 1588687242,
+            "last_rendertime": 1588698039,
             "imgextension": "jpg",
             "isOverlay": false,
             "poititle": "Markers",
