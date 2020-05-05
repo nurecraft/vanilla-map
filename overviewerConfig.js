3a26f0407c233d1beb59a85f25fe5665a085bf04
server-nurecraft <server@nurecraft.ml>
2020-05-05 12:02:53 +0200

    2020-05-05 Day Save

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1588672624",
+        "cacheTag": "1588672968",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "nurecraft - overworld",
-            "last_rendertime": 1588672433,
+            "last_rendertime": 1588672803,
             "imgextension": "jpg",
             "isOverlay": false,
             "poititle": "Markers",
